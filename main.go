736787b9886095039b8ff40afa58b85eea034cab
sky-bai <569531262@qq.com>
2021-11-09 17:51:37 +0800

    打注释

--- main.go
+++ main.go
@@ -6,20 +6,23 @@ import (
 )
 
 func main() {
+	fmt.Println("执行项目")
 	config := sarama.NewConfig()
 	config.Producer.RequiredAcks = sarama.WaitForAll          // 让leader 和 follower都确认
 	config.Producer.Partitioner = sarama.NewRandomPartitioner // 新选出一个partition
 	config.Producer.Return.Successes = true                   // 成功交付的消息将在success channel返回
 
 	msg := &sarama.ProducerMessage{}
-	msg.Topic = "白"
-	msg.Value = sarama.StringEncoder("白")
+	msg.Topic = "bai"
+	msg.Value = sarama.StringEncoder("bai")
 
 	producer, err := sarama.NewSyncProducer([]string{"121.196.163.8:9092"}, config)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	fmt.Println("连接成功")
+
 	defer producer.Close()
 	pid, offset, err := producer.SendMessage(msg)
 	if err != nil {
